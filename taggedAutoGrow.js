185949c1833b27abab9534e666932eb3a5172bb6
Hector Virgen <hvirgen@tagged.com>
2014-01-09 18:16:28 -0800

    Update name in package.json

--- taggedAutoGrow.js
+++ taggedAutoGrow.js
@@ -1,4 +1,4 @@
-/*! tagged-infinite-scroll - v0.0.1 - 2014-01-09 */
+/*! tagged-autogrow - v0.0.1 - 2014-01-09 */
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
